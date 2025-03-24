d392a7e31e1d1ce515065edfd6ffa966639ee036
SJ9211 <jsj921127@naver.com>
2025-03-24 15:48:25 +0900

    New Game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+
+    private float spawnRate = 1.0f;
+    private int score;
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+            UpdateScore(5);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score :" + score;
+    }
+}
+
